7edef2a6877dd79a1cf515c339db0220acbac0ea
SamuelHosken <samuelhosken.o@gmail.com>
2025-04-09 14:45:48 -0300

    Deploy

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { Analytics, getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDK6ysfNMV4H_E5zcjdbClkoI1M-AWrr5k",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "iir-lab-totens.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "iir-lab-totens",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "iir-lab-totens.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "502917274220",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:502917274220:web:3086e183524a85eebc1649",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-QJZYLVC8T6"
+  apiKey: "AIzaSyDK6ysfNMV4H_E5zcjdbClkoI1M-AWrr5k",
+  authDomain: "iir-lab-totens.firebaseapp.com",
+  projectId: "iir-lab-totens",
+  storageBucket: "iir-lab-totens.firebasestorage.app",
+  messagingSenderId: "502917274220",
+  appId: "1:502917274220:web:3086e183524a85eebc1649",
+  measurementId: "G-QJZYLVC8T6"
 };
 
 // Initialize Firebase apenas se não existir
