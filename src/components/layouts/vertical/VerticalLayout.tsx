feec259d656fa66cb31a3fae48df47b689769286
SamuelHosken <samuelhosken.o@gmail.com>
2025-04-26 15:10:10 -0300

    New Design

--- src/components/layouts/vertical/VerticalLayout.tsx
+++ src/components/layouts/vertical/VerticalLayout.tsx
@@ -1,6 +1,6 @@
 import { LayoutProps } from '@/types/layouts';
 import Image from 'next/image';
-import labLogo from '@/public/Lablogo.png';
+import labLogo from '../../../public/Lablogo.png';
 
 interface Slide {
   id: string;
@@ -108,7 +108,7 @@ export function VerticalLayout({ totem, currentTime, isEventoAtual }: VerticalLa
             {/* Header com Logo */}
             <div className="flex justify-center mb-8">
               <Image
-                src={labLogo}
+                src="/Lablogo.png"
                 alt="Lab Camp"
                 width={200}
                 height={80}
